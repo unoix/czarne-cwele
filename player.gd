cc64125ca97fff30653b63a7aad00573332c7011
unoix <antektrap@gmail.com>
2024-07-04 23:16:29 +0200

    prototip

---
+++ player.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@export var speed = 400
+var screen_size
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.DOWN
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
