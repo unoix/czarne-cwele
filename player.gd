c5917b7171b3a6a7d07d0db0114821b7f2e94252
unoix <antektrap@gmail.com>
2024-07-05 00:12:56 +0200

    wiencccej guwna
    lollool
    
    bvrt zrobil kurwa pana serkea
    
    Co-Authored-By: bvrtn <129899925+bvrtn@users.noreply.github.com>

--- player.gd
+++ player.gd
@@ -1,15 +1,16 @@
 extends Node2D
+signal hit
 
 @export var speed = 400
 var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
-
+	hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.DOWN
+	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("up"):
 		velocity.y -= 1
 	if Input.is_action_pressed("left"):
@@ -27,3 +28,21 @@ func _process(delta):
 	
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v= velocity.y > 0
+
+
+func _on_area_2d_body_entered(body):
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
