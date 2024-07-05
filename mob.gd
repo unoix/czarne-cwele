219d3fc0775fd82b27f2007a1d6b385787ff14ee
unoix <antektrap@gmail.com>
2024-07-05 02:33:24 +0200

    gayming
    added sum shit
    
    - player ontrolrl
    - hujd
    - gay foreplay
    - france hate
    - more hot gay sex

---
+++ mob.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite2D.play("default")
+	
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
