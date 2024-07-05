a50919b083569ddb3f716363636e0320da495c77
unoix <antektrap@gmail.com>
2024-07-05 20:08:50 +0200

    joł joł
    - movment
    - no more fyling
    - cwel tworzy tors
    
    Co-Authored-By: bvrtn <129899925+bvrtn@users.noreply.github.com>

---
+++ podejscie-2/scenes/scripts/main.gd
@@ -0,0 +1,9 @@
+extends Node
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Player.position = $StartPos.position
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- podejscie-2/scenes/scripts/player.gd
+++ podejscie-2/scenes/scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 var gravity = ProjectSettings.get("physics/2d/default_gravity")
 @export var WALK_SPEED = 100
-@export var FRICTION = 30
+@export var FRICTION = 3
 @export var JUMP_HEIGHT = 50
 
 func _physics_process(delta):
@@ -17,7 +17,7 @@ func _physics_process(delta):
 	elif velocity.x < 0 :
 		velocity.x += -max(-FRICTION,velocity.x)
 	
-	if Input.is_action_pressed("move_jump"):
+	if Input.is_action_pressed("move_jump") && $RayCast2D.is_colliding():
 		velocity.y -= JUMP_HEIGHT
 
 	move_and_slide()
