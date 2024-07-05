c63d365fde28c9dc0301a5b852e3ec29b7e051a5
unoix <antektrap@gmail.com>
2024-07-05 14:37:53 +0200

    set up project
    - jebaniusieński movemnet
    - jebana podłoga z oppa
    - kurwa bvrt zrób tekstury (jk)

---
+++ podejscie-2/scenes/scripts/player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+var gravity = ProjectSettings.get("physics/2d/default_gravity")
+@export var WALK_SPEED = 100
+@export var FRICTION = 30
+@export var JUMP_HEIGHT = 50
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -WALK_SPEED
+	elif Input.is_action_pressed("move_right"):
+		velocity.x = WALK_SPEED
+	elif velocity.x > 0 :
+		velocity.x -= min(FRICTION,velocity.x)
+	elif velocity.x < 0 :
+		velocity.x += -max(-FRICTION,velocity.x)
+	
+	if Input.is_action_pressed("move_jump"):
+		velocity.y -= JUMP_HEIGHT
+
+	move_and_slide()
+	
