cde30cfa51cbc8bb8a4e76f050b2dc4b57406432
unoix <antektrap@gmail.com>
2024-07-06 18:54:15 +0200

    forgor

--- podejscie-2/scenes/scripts/gun.gd
+++ podejscie-2/scenes/scripts/gun.gd
@@ -1,11 +1,10 @@
 extends Node2D
 
-@export var bullet_scene: PackedScene
-@export var bullet_force = 100
 var can_shoot = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,16 +12,22 @@ func _process(delta):
 	look_at(get_global_mouse_position())
 	if Input.is_action_just_pressed("shoot") && can_shoot:
 		can_shoot = false
+		$AnimatedSprite2D.play("default")
+		await get_tree().create_timer(3/20).timeout
 		shoot()
 		$Cooldown.start()
+		await get_tree().create_timer(3/20).timeout
+		$Label.text = ""
+		
 
 func shoot():
-	var bullet =  bullet_scene.instantiate()
-	$AnimatedSprite2D.play("default")
-	await get_tree().create_timer(0.6).timeout	
-	bullet.start($AnimatedSprite2D/Muzzle.global_position, rotation)
-	get_tree().root.add_child(bullet)
+	$Label.text = "Shot"
+	var object = $Muzzle.get_collider()
+	if object != null:
+		object.queue_free()
 
+
+	
 
 
 func _on_cooldown_timeout():
--- podejscie-2/scenes/scripts/main.gd
+++ podejscie-2/scenes/scripts/main.gd
@@ -1,4 +1,6 @@
 extends Node
+
+@export var op_scene: PackedScene
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Player.position = $StartPos.position
@@ -7,3 +9,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_opp_timer_timeout():
+	$Path2D/PathFollow2D.progress_ratio = randf_range(0.0,1.0)
+	var op = op_scene.instantiate()
+	op.position = $Path2D/PathFollow2D.global_position
+	get_tree().root.add_child(op)
